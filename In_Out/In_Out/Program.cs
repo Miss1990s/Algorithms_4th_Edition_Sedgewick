a2e1f4527ad484ea18e15ecdef1c0dd52f24f902
huo <sinianhe@foxmail.com>
2017-11-30 17:06:01 +0800

    Finish In_Out Module

--- In_Out/In_Out/Program.cs
+++ In_Out/In_Out/Program.cs
@@ -11,9 +11,10 @@ namespace In_Out
     {
         static void Main(string[] args)
         {
+            string fileName = args[0];
             string str;
             while((str = Console.ReadLine())!=null){
-                Console.WriteLine(str+"out");
+                Out.Write<string>(new string[] { str }, fileName);
             }
         }
     }
@@ -37,64 +38,30 @@ namespace In_Out
             return nums.ToArray();
         }
 
-        private static void ParseFile<T>(string name, List<T> list, Func<string,T> parseFunc)
+        private static void ParseFile<T>(string name, List<T> list, Func<string,T> ParseFunc)
         {
             using (StreamReader sr = new StreamReader(name))
             {
                 string line;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    var strs = line.Trim().Split(' ');
-                    foreach (var st in strs)
+                    T num = ParseFunc(line.Trim());
                     {
-                        T num = parseFunc(st);
-                        {
-                            list.Add(num);
-                        }
+                        list.Add(num);
                     }
                 }
             }
         }
     }
 
-    //class Out
-    //{
-    //    public static void Write(int[]array, string name)
-    //    {
-    //        List<Int32> nums = new List<int>();
-    //        ParseFile(name, nums, x => Int32.Parse(x));
-    //        return nums.ToArray();
-    //    }
-    //    public static void Write(double[]array, string name)
-    //    {
-    //        List<Double> nums = new List<Double>();
-    //        ParseFile(name, nums, x => Double.Parse(x));
-    //        return nums.ToArray();
-    //    }
-    //    public static void Write(string[] array, string name)
-    //    {
-    //        List<String> nums = new List<String>();
-    //        ParseFile(name, nums, x => x);
-    //        return nums.ToArray();
-    //    }
-
-    //    private static void ParseFile<T>(string name, List<T> list, Func<string, T> parseFunc)
-    //    {
-    //        using (StreamReader sr = new StreamReader(name))
-    //        {
-    //            string line;
-    //            while ((line = sr.ReadLine()) != null)
-    //            {
-    //                var strs = line.Trim().Split(',');
-    //                foreach (var st in strs)
-    //                {
-    //                    T num = parseFunc(st);
-    //                    {
-    //                        list.Add(num);
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
+    class Out
+    {
+        public static void Write<T>(T[] array, string name)
+        {
+            using (StreamWriter fs = new StreamWriter(name,true))
+            {
+                Array.ForEach<T>(array, x => fs.WriteLine(x));
+            }
+        }
+    }
 }
